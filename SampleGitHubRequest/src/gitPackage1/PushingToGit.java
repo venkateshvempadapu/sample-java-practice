ded91a29d30c1d01db99802eb3ff297eec9f301d
Bheemsh <Bheemsh@Bheemsh-PC>
2019-03-02 15:30:15 +0530

    commiting on sample java practice to Git from Venky

---
+++ SampleGitHubRequest/src/gitPackage1/PushingToGit.java
@@ -0,0 +1,11 @@
+package gitPackage1;
+
+public class PushingToGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Sample Projct for GitHub Pushing Code");
+		
+	}
+
+}
